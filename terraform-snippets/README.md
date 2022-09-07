9f30c05945871affdc8b41697dfc56eefc2e393f
David Levy <david.g.levy@gmail.com>
2022-09-08 07:47:19 +1000

    Adding terraform creation, NB still need to get cluster deps working properly.

---
+++ terraform-snippets/README.md
@@ -0,0 +1,9 @@
+# Terraform Snippets
+
+This is a collection of folders (not modules)
+where I can add extensions to the base terraform
+template which are quickly deployed and tested without
+needing to spin up an entire workspace.
+
+Once I get better at building modules, this is also likely a good way to
+encapsulate chunks of code and infrastrucutre for pipelines.
---
+++ terraform-snippets/nlp-cluster/main.tf
@@ -0,0 +1,65 @@
+terraform {
+  required_providers {
+    databricks = {
+      source = "databricks/databricks"
+      version = ">= 1.2.1"
+    }
+  }
+}
+  # TODO Remove dlevy and switch to username from az login
+variable "workspace_url" {
+  description = "The workspace URL"
+  type = string
+  sensitive = true
+}
+provider "databricks" {
+  host = var.workspace_url
+}
+
+data "databricks_spark_version" "latest_lts" {
+  long_term_support = true
+}
+
+resource "databricks_cluster" "shared_autoscaling" {
+  cluster_name            = "NLP Cluster 2"
+  spark_version           = data.databricks_spark_version.latest_lts.id
+  node_type_id            = "Standard_D8ds_v4"
+  data_security_mode      = "SINGLE_USER"
+  single_user_name        = "dlevy@databricks.com"
+  autotermination_minutes = 60
+
+  // Add libraries
+  // TODO Make Wheel from deps
+  library {
+    pypi {
+      package = "pdf2image"
+    }
+  }
+  library {
+    pypi {
+      package = "git+https://github.com/facebookresearch/detectron2.git@v0.5#egg=detectron2"
+
+    }
+  }
+  library {
+    pypi {
+      package = "layoutparser[ocr]"
+    }
+  }
+  library {
+    pypi {
+      package = "torchvision"
+    }
+  }
+
+  // Add init script
+  init_scripts {
+    dbfs {
+      destination = "dbfs:/scripts/cluster_init.sh"
+    }
+  }
+
+  // Set number of workers (2)
+  num_workers = 2
+
+}
