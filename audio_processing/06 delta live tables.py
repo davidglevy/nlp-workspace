70bc0fcd601dde48d1a737402a2d26522cfecd36
davidglevy <davidglevy>
2022-10-07 01:34:21 +0000

    DLT working, next up pandas UDF for invocation

--- audio_processing/06 delta live tables.py
+++ audio_processing/06 delta live tables.py
@@ -1,18 +1,41 @@
 # Databricks notebook source
-from pyspark.sql.types import StructType, StructField, StringType, MapType, ArrayType, BinaryType
+from pyspark.sql.types import StructType, StructField, StringType, MapType, ArrayType, BinaryType, IntegerType
 from urllib import request
+import dlt
 
-url_list = [
-    {"url": "https://www.americanrhetoric.com/mp3clipsXE/barackobama/barackobamainauguraladdressARXE.mp3"},
-    {"url": "https://www.americanrhetoric.com/mp3clips/politicalspeeches/jfkinauguralsurround.mp3"}
-]
 
-schema_load = StructType([
-    StructField("url", StringType(), False),
-    StructField("file_name", StringType(), True)
-])
+@dlt.view
+def nlp_urls():
+    url_list = [
+        {"url": "https://www.americanrhetoric.com/mp3clips/barackobama/barackobamafederalplaza.mp3", "title": "Federal Plaza Address Opposing the War in Iraq", "date": "02 Oct 2002"},
+        {"url": "https://www.americanrhetoric.com/mp3clipsXE/barackobama/barackobama2004dncARXE.mp3", "title": "Democratic National Convention Keynote Speech", "date": "27 Jul 2004"},
+        {"url": "https://www.americanrhetoric.com/mp3clips/barackobama/barackobamasenatespeechohiovotecounting.mp3", "title": "Senate Speech on Ohio Electoral Vote", "date": "06 Jan 2005"},
+        {"url": "https://www.americanrhetoric.com/mp3clips/barackobama/barackobamaknoxcommencement.mp3", "title": "Knox College Commencement Speech", "date": "04 Jun 2005"},
+        {"url": "https://www.americanrhetoric.com/mp3clips/barackobama/barackobamasenatespeechrosaparks.mp3", "title": "Senate Speech Honoring the Life of Rosa Parks", "date": "25 Oct 2005"},
+        {"url": "https://www.americanrhetoric.com/mp3clipsXE/barackobama/barackobamasenatefloorspeechpatriotactARXE.mp3", "title": "Senate Speech on the PATRIOT Act", "date": "15 Dec 2005"},
+        {"url": "https://www.americanrhetoric.com/mp3clips/barackobama/barackobamasenatespeechcorettascottking.mp3", "title": "Senate Speech Honoring the Life of Coretta Scott King", "date": "31 Jan 2006"},
+        {"url": "https://www.americanrhetoric.com/mp3clipsXE/barackobama/barackobamasenatespeechvotingrightsactARXE.mp3", "title": "Senate Speech on Voting Rights Act Renewal", "date": "20 Jul 2006"},
+        {"url": "https://www.americanrhetoric.com/mp3clips/barackobama/barackobamaexploratorycommittee.mp3", "title": "Presidential Exploratory Committee Announcement", "date": "16 Jan 2007"},
+        {"url": "https://www.americanrhetoric.com/mp3clips/barackobama/barackobamacandidacyannouncement.mp3", "title": "Presidential Candidacy Announcement", "date": "10 Feb 2007"},
+        {"url": "https://www.americanrhetoric.com/mp3clips/barackobama/barackobamabrownchapel.mp3", "title": "Brown Chapel Speech", "date": "04 Mar 2007"},
+        {"url": "https://www.americanrhetoric.com/mp3clips/barackobama/barackobamasenatespeechiraqfederalism.mp3", "title": "Senate Speech on Iraq Federalism Amendment", "date": "13 Mar 2007"},
+        {"url": "https://www.americanrhetoric.com/mp3clips/barackobama/barackobamasenatespeechiraqwar4years.mp3", "title": "Senate Speech on Iraq War After 4 Years", "date": "21 Mar 2007"},
+        {"url": "https://www.americanrhetoric.com/mp3clips/barackobama/barackobamasenatespeechimmigrationreform.mp3", "title": "Senate Speech on Comprehensive Immigration Reform", "date": "23 May 2007"},
+        {"url": "https://www.americanrhetoric.com/mp3clips/barackobama/barackobamawilsoncenter.mp3", "title": "Woodrow Wilson Center Speech", "date": "01 Aug 2007"},
+        {"url": "https://www.americanrhetoric.com/mp3clipsXE/barackobama/barackobamajeffersonjacksondinnerARXE.mp3", "title": "Speech at the Jefferson-Jackson Dinner", "date": "10 Nov 2007"},
+        {"url": "https://www.americanrhetoric.com/mp3clipsXE/barackobama/barackobamaiowacaucusspeechvictoryARXE.mp3", "title": "Iowa Caucus Victory Speech", "date": "03 Jan 2008"}#,
+    #    {"url": "https://www.americanrhetoric.com/mp3clipsXE/barackobama/barackobamainauguraladdressARXE.mp3"},
+    #    {"url": "https://www.americanrhetoric.com/mp3clips/politicalspeeches/jfkinauguralsurround.mp3"}
+    ]    
+    schema_load = StructType([
+        StructField("title", StringType(), False),
+        StructField("url", StringType(), False),
+        StructField("date", StringType(), False),
+        StructField("file_name", StringType(), True)
+    ])
+    
+    return spark.createDataFrame(url_list, schema_load)
 
-url_list_df = spark.createDataFrame(url_list, schema_load)
 
 # COMMAND ----------
 
@@ -24,9 +47,6 @@ from urllib import request
 def extract_file_name(input):
     result = re.search(".*\\/([^\\/?]+)", input)
     return result.group(1)
-
-print(extract_file_name("https://www.americanrhetoric.com/mp3clips/politicalspeeches/jfkinaugural2.mp3?abc=123&dfg=456"))
-
 
 def download_audio(input, file_name):
     
@@ -79,20 +99,18 @@ download_audio_udf = udf(download_audio, schema)
 
 # COMMAND ----------
 
-downloaded_df = url_list_df.withColumn("downloaded", download_audio_udf(col("url"), col("file_name")))
-
-# COMMAND ----------
-
-final_df = downloaded_df.select("downloaded.path", "downloaded.file_name", "downloaded.url", "downloaded.site", "downloaded.scheme", "downloaded.query", "downloaded.content")
-
-# COMMAND ----------
-
-final_df.cre
-
-# COMMAND ----------
-
-# MAGIC %sql
-# MAGIC CREATE OR REFRESH LIVE TABLE nlp_raw
-# MAGIC 
-# MAGIC 
-# MAGIC #final_df.write.format("delta").mode("overwrite").option("overwriteSchema", True).saveAsTable("nlp.audio.audio_raw")
+@dlt.table
+def audio_raw():
+    downloaded_df = dlt.read("nlp_urls").repartition(20).withColumn("downloaded", download_audio_udf(col("url"), col("file_name")))
+    final_df = downloaded_df.select(
+        "title",
+        "date",
+        "downloaded.path", 
+        "downloaded.file_name", 
+        "downloaded.url", 
+        "downloaded.site", 
+        "downloaded.scheme", 
+        "downloaded.query", 
+        "downloaded.content"
+    )
+    return final_df
---
+++ audio_processing/07 DLT - Convert to Text v2.py
@@ -0,0 +1,100 @@
+# Databricks notebook source
+# MAGIC %pip install azure-cognitiveservices-speech pydub
+
+# COMMAND ----------
+
+import azure.cognitiveservices.speech as speechsdk
+from azure.cognitiveservices.speech import SpeechRecognizer, SpeechConfig
+from pydub import AudioSegment
+import tempfile
+#from urllib import request
+from io import BytesIO
+#import requests
+import json
+import time
+
+# COMMAND ----------
+
+def convert_to_text(input, key):
+    
+    # TODO Add type conversion to pipeline.
+    sound_file = AudioSegment.from_mp3(BytesIO(input))
+
+    exported = BytesIO()
+    sound_file.export(exported, "wav")
+    converted = exported.read()
+
+    
+    
+    with tempfile.NamedTemporaryFile() as tmp:
+        with open(tmp.name, 'wb') as f:
+            f.write(converted)
+        
+        speech_config = speechsdk.SpeechConfig(subscription=key, region="AustraliaEast")
+        audio_input = speechsdk.AudioConfig(filename=tmp.name)
+        speech_recognizer = speechsdk.SpeechRecognizer(speech_config=speech_config, audio_config=audio_input)
+
+    done = False
+
+    all_results = []
+
+    def handle_final_result(evt):
+        if (evt.result.reason == speechsdk.ResultReason.RecognizedSpeech):
+            text = evt.result.text
+            print(f"RECOGNIZED: {text}")
+            all_results.append(evt.result.text)
+        else:
+            print("Ignoring non-success result {}".format(evt))
+    
+    def stop_cb(evt):
+        """callback that stops continuous recognition upon receiving an event `evt`"""
+        print('CLOSING on {}'.format(evt))
+        speech_recognizer.stop_continuous_recognition()
+        nonlocal done
+        done = True
+
+    # Connect callbacks to the events fired by the speech recognizer
+    # Commented this out - noisy
+    #speech_recognizer.recognizing.connect(lambda evt: print('RECOGNIZING: {}'.format(evt)))
+    speech_recognizer.recognized.connect(handle_final_result)
+    speech_recognizer.session_started.connect(lambda evt: print('SESSION STARTED: {}'.format(evt)))
+    speech_recognizer.session_stopped.connect(lambda evt: print('SESSION STOPPED {}'.format(evt)))
+    speech_recognizer.canceled.connect(lambda evt: print('CANCELED {}'.format(evt)))
+    # stop continuous recognition on either session stopped or canceled events
+    speech_recognizer.session_stopped.connect(stop_cb)
+    speech_recognizer.canceled.connect(stop_cb)
+
+    # Start continuous speech recognition
+    speech_recognizer.start_continuous_recognition()
+    while not done:
+        time.sleep(.5)
+        
+    return all_results
+
+# COMMAND ----------
+
+from pyspark.sql.types import StructType, StructField, ArrayType, BinaryType, StringType
+from pyspark.sql.functions import udf, col, lit, array_join
+
+convert_to_text_udf = udf(convert_to_text, ArrayType(StringType()))
+
+# COMMAND ----------
+
+import dlt
+
+# Get SaaS Key
+key = dbutils.secrets.get("saas_keys", "azure_speech")
+print(f"The key is [{key}]")
+
+
+    
+
+# COMMAND ----------
+
+#TODO If we want to keep the "parts" before joining back together, re-instate this from the v1 version.
+
+@dlt.table
+def nlp_converted():
+    df_raw = dlt.read("audio_raw")
+    df_translated = df_raw.withColumn("speech_as_text", array_join(convert_to_text_udf(col("content"), lit(key)), " "))
+    return df_translated
